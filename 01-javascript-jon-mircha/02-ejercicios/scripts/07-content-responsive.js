8a8c1c709dd362846738391ef9ae257f032cd184
dasherrandom <alansitoruiz7@gmail.com>
2026-08-28 17:15:37 -0400

    refactor(js): change in the code of the module changeContent

--- 01-javascript-jon-mircha/02-ejercicios/scripts/07-content-responsive.js
+++ 01-javascript-jon-mircha/02-ejercicios/scripts/07-content-responsive.js
@@ -1,10 +1,16 @@
 export function contentResponsive(idContainer, mediaQuerie, desktopContent, mobileContent) {
   const $container = document.getElementById(idContainer)
   const defineContent = window.matchMedia(mediaQuerie);
-  console.log(defineContent)
-  if (defineContent.matches) {
-    $container.innerHTML = desktopContent
-  } else {
-    $container.innerHTML = mobileContent
+  const changeContent = () => {
+    console.log(defineContent)
+    if (defineContent.matches) {
+      $container.innerHTML = desktopContent
+    } else {
+      $container.innerHTML = mobileContent
+    }
   }
+  changeContent()
+  defineContent.addEventListener("change", () => {
+    changeContent()
+  })
 }
